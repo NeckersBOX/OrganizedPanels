bcce8acef2e735a6d8fd88c55e99187a8360747c
Davide Francesco Merico <hds619@gmail.com>
2017-02-26 23:06:18 +0100

    Remove jQuery dep, Add bookmarks, history and closed tabs panel

--- js/organized-panels.js
+++ js/organized-panels.js
@@ -1,22 +1,46 @@
-$(() => {
+/*
+ * Author: Davide Francesco Merico
+ */
+
+/* When the document is fully loaded add Bookmaks, History and Closed Tabs Panels */
+document.addEventListener ('DOMContentLoaded', () => {
   chrome.bookmarks.getTree (showBookmark);
+
+  chrome.history.search ({
+    text: '',
+    maxResults: 10
+  }, showHistory);
+
+  chrome.sessions.getRecentlyClosed ({
+    maxResults: 10
+  }, showClosedTabs);
 });
 
-const showBookmark = bookmarkTreeNode => serializeBookmarks (bookmarkTreeNode).forEach (bookmark =>
-  $('#container').append (
-    '<div class="panel bookmark">' +
-    ' <h1>' + bookmark.name + '</h1>' +
-    ' <ul>' + bookmark.links.reduce ((formattedHtml, link) =>
-      formattedHtml +
-      '<li>' +
-      ' <a target="_blank" href="' + link.url + '">' + link.title + '</a>' +
-      '</li>', ''
-    ) +
-    ' </ul>' +
-    '</div>'
-  )
-);
+/* Add the panel in the 'container' div, working a bit directly with the DOM.
+ * This function replace the dependency from jquery's append () function too.
+ * ( Note: if you replace this function with a simple $('#container').append ()
+ *   when the extension load the "closed tabs" chrome will print an error because
+ *   of this: https://developer.chrome.com/extensions/contentSecurityPolicy#relaxing-inline-script )
+ */
+const addPanel = (type, title, links) => {
+  const panelDOM = document.createElement ('div');
+  panelDOM.className = 'panel ' + type;
 
+  const panelTitleDOM = document.createElement ('h1');
+  panelTitleDOM.innerText = title;
+
+  const panelListDOM = document.createElement ('ul');
+  panelListDOM.innerHTML = links;
+
+  panelDOM.appendChild (panelTitleDOM);
+  panelDOM.appendChild (panelListDOM);
+  document.getElementById ('container').appendChild (panelDOM);
+};
+
+/* Serialize the bookmarks tree supplied by chrome.bookmarks.getTree ()
+ * Each folder, and sub-folder, is pushed out from its context\node with
+ * only name and children links.
+ */
 const serializeBookmarks = bookmarkTreeNode => {
   let serialized = [];
 
@@ -33,3 +57,35 @@ const serializeBookmarks = bookmarkTreeNode => {
 
   return serialized.filter (node => node.links.length);
 };
+
+/* Add the bookmark panel */
+const showBookmark = bookmarkTreeNode => serializeBookmarks (bookmarkTreeNode).forEach (bookmark =>
+  addPanel ('bookmark', bookmark.name, bookmark.links.reduce ((formattedHtml, link) =>
+      formattedHtml +
+      '<li>' +
+      ' <a target="_blank" href="' + link.url + '">' + link.title + '</a>' +
+      '</li>', ''
+  ))
+);
+
+/* Add the history panel */
+const showHistory = historyItems =>
+  addPanel ('history', 'Browser History', historyItems.reduce ((formattedHtml, link) =>
+    formattedHtml +
+    '<li>' +
+    ' <a target="_blank" href="' + link.url + '">' +
+      (link.title.length ? link.title : link.url) +
+    '</a>' +
+    '</li>', ''
+  ));
+
+/* Add the closed tabs panel */
+const showClosedTabs = closedTabs =>
+  addPanel ('closed-tabs', 'Closed Tabs', closedTabs.reduce ((formattedHtml, closedTab) =>
+    formattedHtml +
+    '<li>' +
+    ' <a target="_blank" href="' + closedTab.tab.url + '">' +
+      (closedTab.tab.title.length ? closedTab.tab.title : closedTab.tab.url) +
+    '</a>' +
+    '</li>', ''
+  ));
